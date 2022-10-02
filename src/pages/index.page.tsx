660f6a5b32a19a60cc24dc96fe4e45dce19c6acd
Hannan Hafeez <30556614+hannanhafeez@users.noreply.github.com>
2022-10-02 06:21:29 +0500

    Tailwind config added!
    Signed-off-by: Hannan Hafeez <30556614+hannanhafeez@users.noreply.github.com>

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import {
-  Center,
-  useColorMode,
-  Tooltip,
-  IconButton,
-  SunIcon,
-  MoonIcon,
-  Image,
-  HStack,
-  Text,
-  Heading,
-  Box,
-  Link,
-  VStack,
-  Button,
-  AspectRatio,
+	Center,
+	useColorMode,
+	Tooltip,
+	IconButton,
+	SunIcon,
+	MoonIcon,
+	Image,
+	HStack,
+	Text,
+	Heading,
+	Box,
+	Link,
+	VStack,
+	Button,
+	AspectRatio,
 } from "native-base";
 
 import ExportedImage from "next-image-export-optimizer";
@@ -23,95 +23,95 @@ import NextJsImage from '../../public/images/nextjs-logo.png'
 
 // Start editing here, save and see your changes.
 export default function App() {
-  return (
-    <Center
-      flex={1}
-      _dark={{ bg: "blueGray.900" }}
-      _light={{ bg: "blueGray.50" }}
-    >
-      <VStack alignItems="center" space="md">
-        <HStack alignItems="center" space="2xl">
-          <AspectRatio w={24} ratio={1.66}>
-            {/* <Image
-              source={{ uri: "images/nextjs-logo.png" }}
-              alt="NextJS Logo"
-              resizeMode="contain"
-            /> */}
-            <div style={{height: '100%', width:'100%'}}>
-              <ExportedImage alt='abc' src={NextJsImage} layout='fill' objectFit="contain"/>
-            </div>
-          </AspectRatio>
-          <Text fontSize="4xl">+</Text>
-          <Image
-            source={{ uri: "images/nativebase-logo.svg" }}
-            alt="NativeBase Logo"
-            size={24}
-            resizeMode="contain"
-          />
-        </HStack>
-        <Heading>Welcome to NativeBase</Heading>
-        <Text>
-          Edit{" "}
-          <Box
-            _text={{
-              fontFamily: "monospace",
-              fontSize: "sm",
-            }}
-            px={2}
-            py={1}
-            _dark={{ bg: "blueGray.800" }}
-            _light={{ bg: "blueGray.200" }}
-          >
-            src/pages/index.js
-          </Box>{" "}
-          and save to reload.
-        </Text>
-        <HStack alignItems="center" space="sm">
-          <Link href="https://nextjs.org/docs/getting-started" isExternal>
-            <Text
-              _light={{ color: "gray.700" }}
-              _dark={{ color: "gray.400" }}
-              underline
-              fontSize={"xl"}
-            >
-              Learn NextJS
-            </Text>
-          </Link>
-          <Text>/</Text>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-        </HStack>
-      </VStack>
-      <ColorModeSwitch />
-      <Link mt="6" href="https://docs.nativebase.io" isExternal>
-        <Button variant="outline" colorScheme="coolGray">
-          View Repo
-        </Button>
-      </Link>
-    </Center>
-  );
+	return (
+		<Center
+			flex={1}
+			_dark={{ bg: "blueGray.900" }}
+			_light={{ bg: "blueGray.50" }}
+		>
+			<VStack alignItems="center" space="md">
+				<HStack alignItems="center" space="2xl">
+					<AspectRatio w={24} ratio={1.66}>
+						{/* <Image
+							source={{ uri: "images/nextjs-logo.png" }}
+							alt="NextJS Logo"
+							resizeMode="contain"
+						/> */}
+						<div className='w-full h-full'>
+							<ExportedImage alt='abc' src={NextJsImage} layout='fill' objectFit="contain"/>
+						</div>
+					</AspectRatio>
+					<Text fontSize="4xl">+</Text>
+					<Image
+						source={{ uri: "images/nativebase-logo.svg" }}
+						alt="NativeBase Logo"
+						size={24}
+						resizeMode="contain"
+					/>
+				</HStack>
+				<Heading>Welcome to NativeBase</Heading>
+				<Text>
+					Edit{" "}
+					<Box
+						_text={{
+							fontFamily: "monospace",
+							fontSize: "sm",
+						}}
+						px={2}
+						py={1}
+						_dark={{ bg: "blueGray.800" }}
+						_light={{ bg: "blueGray.200" }}
+					>
+						src/pages/index.js
+					</Box>{" "}
+					and save to reload.
+				</Text>
+				<HStack alignItems="center" space="sm">
+					<Link href="https://nextjs.org/docs/getting-started" isExternal>
+						<Text
+							_light={{ color: "gray.700" }}
+							_dark={{ color: "gray.400" }}
+							underline
+							fontSize={"xl"}
+						>
+							Learn NextJS
+						</Text>
+					</Link>
+					<Text>/</Text>
+					<Link href="https://docs.nativebase.io" isExternal>
+						<Text color="primary.500" underline fontSize={"xl"}>
+							Learn NativeBase
+						</Text>
+					</Link>
+				</HStack>
+			</VStack>
+			<ColorModeSwitch />
+			<Link mt="6" href="https://docs.nativebase.io" isExternal>
+				<Button variant="outline" colorScheme="coolGray">
+					View Repo
+				</Button>
+			</Link>
+		</Center>
+	);
 }
 // Color Switch Component
 function ColorModeSwitch() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <Tooltip
-      label={colorMode === "dark" ? "Enable light mode" : "Enable dark mode"}
-      placement="bottom right"
-      openDelay={300}
-      closeOnClick={false}
-    >
-      <IconButton
-        position="absolute"
-        top={12}
-        right={8}
-        onPress={toggleColorMode}
-        icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
-        accessibilityLabel="Color Mode Switch"
-      />
-    </Tooltip>
-  );
+	const { colorMode, toggleColorMode } = useColorMode();
+	return (
+		<Tooltip
+			label={colorMode === "dark" ? "Enable light mode" : "Enable dark mode"}
+			placement="bottom right"
+			openDelay={300}
+			closeOnClick={false}
+		>
+			<IconButton
+				position="absolute"
+				top={12}
+				right={8}
+				onPress={toggleColorMode}
+				icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+				accessibilityLabel="Color Mode Switch"
+			/>
+		</Tooltip>
+	);
 }
